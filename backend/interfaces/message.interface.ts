5d306a7ec5dbe51e496c6de24aeb5f339f47cab0
Muhammad Ali <muhammadali.hisar@gmail.com>
2023-12-17 08:45:49 +0500

    message interface added and producer class updated

---
+++ backend/interfaces/message.interface.ts
@@ -0,0 +1,5 @@
+export interface IMessage {
+  room: string;
+  message: string;
+  sender: string
+}
---
+++ backend/kafka/chat.producer.ts
@@ -0,0 +1,38 @@
+import { Kafka, Message, Producer, ProducerBatch, TopicMessages } from 'kafkajs'
+import { kafka } from "../config/kafka.client";
+import { IMessage } from '../interfaces/message.interface';
+
+export default class ProducerFactory {
+  private producer: Producer
+
+  constructor() {
+    this.producer = this.createProducer()
+  }
+
+  public async start(): Promise<void> {
+    try {
+      await this.producer.connect()
+    } catch (error) {
+      console.log('Error connecting the producer: ', error)
+    }
+  }
+
+  public async stop(): Promise<void> {
+    await this.producer.disconnect()
+  }
+
+  async  sendMessage(message: IMessage) {
+    console.log(`Producer sent message: ${message} to topic: ${message.room}`);
+    await this.producer.send({
+      topic: message.room,
+      messages: [
+        { value: JSON.stringify({ message: message.message, sender: message.sender }), timestamp: Date.now().toString() },
+      ],
+    });
+  }
+ 
+
+  private createProducer() : Producer {
+     return kafka.producer()
+  }
+}
