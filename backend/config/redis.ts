c9a07088e22750818109e5e82cf1c5b5bb9dfefa
Muhammad Ali <muhammadali.hisar@gmail.com>
2023-12-30 13:44:58 +0500

    save and get online users in redis

--- backend/config/redis.ts
+++ backend/config/redis.ts
@@ -1,4 +1,5 @@
 import { createClient, RedisClientType } from "redis";
+import { IUser } from "../interfaces/users.interface";
 // const redisClient = createClient({ url: "redis://localhost:6379" })
 // .on('error', err => console.log('Redis Client Error', err))
 
@@ -22,5 +23,41 @@ export class RedisClient {
     await this.redisC.disconnect();
   };
 
- 
+  public async getOnlineUsers(
+    pattern: string
+  ) {
+   let users:any[]=[]
+    try {
+      //   for await (const key of this.redisC.scanIterator({
+      //     MATCH: pattern,
+      //     COUNT: 50,
+      // }))
+      for await (const data of this.redisC.hScanIterator(pattern))
+       {
+       console.log("KEY",data.value)
+       users.push(JSON.parse(data.value))
+       // const user=await this.redisC.sMembers(key)
+      //  if(value){
+      //  console.log(user) 
+      //    users=[...users, ...user]
+      //  }
+       }
+      return users;
+    } catch (error) {
+      throw error;
+    }
+  }
+  public saveUser=async(user:IUser, status:string)=>{
+    await this.redisC.hSet(
+      `online_users`,
+      user.id,
+      JSON.stringify(
+      {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        status: status
+      })
+    );
+  }
 }
--- backend/services/chat.service.ts
+++ backend/services/chat.service.ts
@@ -3,54 +3,59 @@ import { ChatProducer } from "./chat.producer";
 import { createTopics } from "./admin";
 import { SocketIO } from "../config/socketio";
 import { Server } from "http";
-import {RedisClient} from "../config/redis";
+import { RedisClient } from "../config/redis";
 import { getMainRoomMessages } from "../controllers/message.controller";
 import { validateToken } from "../middleware/auth.middleware";
 
-export const startChatServices = async (http:Server,redisClient:RedisClient) => {
-    const io = new SocketIO(http, redisClient).getIO();
-    const chatConsumer = new ChatConsumer(io);
-    const chatProducer = new ChatProducer();
-    await chatProducer.start();
-    await chatConsumer.startConsumer();
-  
-    io.use(async(socket, next) => {
-      //TODO: Verify token and get user detail
-        const token = socket.handshake.auth.token;
-        console.log("socket token",socket.handshake)
-        if (!token) {
-          return next(new Error("invalid token"));
-        }
-        const decoded=await validateToken(token)
-        if(decoded ===null) {
-          return next(new Error("invalid token"));
-        }
-        socket.data.user=decoded
-        console.log("Socket User",socket.data.user)
-        next();
-      });
-    io.on("connection", async(socket) => {
-      const count = io.engine.clientsCount;
-      console.log("Connected Users", count);
-      
-      socket.on("subscribe", async (topic) => {
-        createTopics(topic);
-        socket.join(topic);
-        await chatConsumer.subscribeToTopic(topic);
-        const messages=await getMainRoomMessages();
-        socket.emit("messageHistory",messages)
-      });
-  
-      socket.on("chatMessage", async (msgObj) => {
-        const { senderId, room, message } = msgObj;
-         await chatProducer.sendMessage({senderId: senderId,room,message})
-      });
+export const startChatServices = async (
+  http: Server,
+  redisClient: RedisClient
+) => {
+  const io = new SocketIO(http, redisClient).getIO();
+  const chatConsumer = new ChatConsumer(io);
+  const chatProducer = new ChatProducer();
+  await chatProducer.start();
+  await chatConsumer.startConsumer();
 
-      socket.onAny((event, ...args) => {
-        console.log(event, args);
-      });
-      socket.on("disconnect", () => {
-        console.log("User disconnected");
-      });
+  io.use(async (socket, next) => {
+    //TODO: Verify token and get user detail
+    const token = socket.handshake.auth.token;
+    if (!token) {
+      return next(new Error("invalid token"));
+    }
+    const decoded = await validateToken(token);
+    if (decoded === null) {
+      return next(new Error("invalid token"));
+    }
+    socket.data.user = decoded;
+    next();
+  });
+  io.on("connection", async (socket) => {
+    const count = io.engine.clientsCount;
+    console.log("Connected Users", count);
+    const user = socket.data.user;
+    await redisClient.saveUser(user, "online");
+    const onlineUsers = await redisClient.getOnlineUsers("online_users");
+    console.log("ONline Users", onlineUsers);
+    socket.emit("users", onlineUsers);
+    socket.on("subscribe", async (topic) => {
+      createTopics(topic);
+      socket.join(topic);
+      await chatConsumer.subscribeToTopic(topic);
+      const messages = await getMainRoomMessages();
+      socket.emit("messageHistory", messages);
     });
-  };
+
+    socket.on("chatMessage", async (msgObj) => {
+      const { senderId, room, message } = msgObj;
+      await chatProducer.sendMessage({ senderId: senderId, room, message });
+    });
+
+    socket.onAny((event, ...args) => {
+      console.log(event, args);
+    });
+    socket.on("disconnect", () => {
+      console.log("User disconnected");
+    });
+  });
+};
