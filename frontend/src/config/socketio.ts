59e76f04c36ac435e6d7cf493eb085ab90de79de
Muhammad Ali <muhammadali.hisar@gmail.com>
2023-12-31 13:46:18 +0500

    types added to socketio

--- frontend/src/config/socketio.ts
+++ frontend/src/config/socketio.ts
@@ -1,10 +1,23 @@
 import { io, Socket } from "socket.io-client";
-let socket: Socket;
+import { IMessage } from "../interfaces/message.interface";
+import { IListUser } from "@/interfaces/auth.interfaces";
+interface ServerToClientEvents {
+  messageHistory: (messages: IMessage[]) => void;
+  message: (message: IMessage) => void;
+  userConnected: (user: IListUser) => void;
+  users: (users:IListUser[])=>void
+  userDisconnected: (userId: number) => void;
+  reconnect:()=>void
+}
 
+interface ClientToServerEvents {
+  subscribe: (roomName: string) => void;
+}
+let socket: Socket<ServerToClientEvents, ClientToServerEvents>;
 
 export const initSocket = () => {
   if (!socket) {
-    socket = io(process.env.NEXT_PUBLIC_SOCKET_API!,  { autoConnect: false });
+    socket = io(process.env.NEXT_PUBLIC_SOCKET_API!, { autoConnect: false , transports: ['websocket'], upgrade: false});
   }
   return socket;
 };
