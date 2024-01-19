0021ef720672f5a39b16e1dd91996c90a1cd0194
Muhammad Ali <muhammadali.hisar@gmail.com>
2024-01-19 20:49:27 +0500

    update message method added to redis service

--- backend/services/message/redis-message.service.ts
+++ backend/services/message/redis-message.service.ts
@@ -18,10 +18,40 @@ export class RedisMessageService implements IObserver {
     this.redisClient.rPush(roomName, JSON.stringify(message));
   }
   saveMainChatMessage(message: IMessage) {
-    this.redisClient.rPush(`main_chat_messages`, JSON.stringify(message));
+    this.redisClient.rPush(
+      `main_chat_messages:${message.senderId}`,
+      JSON.stringify(message)
+    );
   }
   update(message: IMessage, topic: string): void {
     if (topic === "chat") this.saveMainChatMessage(message);
     else this.savePrivateChatMessage(message);
+  }
+  async updateMainChatMessage(
+    messageId: number,
+    messageClientId: number,
+    senderId: number
+  ) {
+    const messages = await this.redisClient.lRange(
+      `main_chat_messages:${senderId}`,
+      0,
+      -1
+    );
+
+    // Find the index of the message to update
+    const indexToUpdate = messages.findIndex(
+      (msg) => JSON.parse(msg).messageClientId === messageClientId
+    );
+
+    // Update the message at the identified index
+    if (indexToUpdate !== -1) {
+      const updatedMessage = JSON.parse(messages[indexToUpdate]);
+
+      this.redisClient.lSet(
+        `main_chat_messages:${senderId}`,
+        indexToUpdate,
+        JSON.stringify({ ...updatedMessage, id: messageId })
+      );
+    }
   }
 }
