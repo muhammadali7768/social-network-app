352879f580cc7d11868d92658ca203718e0a128e
Muhammad Ali <muhammadali.hisar@gmail.com>
2024-01-21 22:02:14 +0500

    moved useEffects to a hook

--- frontend/src/pages/chat-window.tsx
+++ frontend/src/pages/chat-window.tsx
@@ -12,7 +12,9 @@ import { IMessage } from "@/interfaces/message.interface";
 import MainChatItem from "@/components/chat/MainChatItem";
 import NavigateService from "@/services/navigate";
 import { getNewTokenByRefreshToken } from "@/config/axios";
+import { useChatEffects } from "@/hooks/useChatEffects";
 export default function ChatWindow() {
+  useChatEffects()
   const usersList = useUserStore((state) => state.usersList);
   const setUsersList = useUserStore((state) => state.setUsersList);
   const updateUserMessages = useUserStore((state) => state.updateUserMessages);
@@ -23,146 +25,15 @@ export default function ChatWindow() {
     setMainChatMessages,
     updateActiveChatMessages,
     updateMainChatMessages,
+    updateMainMessage,
+    updatePrivateMessage
   } = useChatStore();
   const user = useUserStore((state) => state.user);
   const [activeIndex, setActiveIndex] = useState(0);
 
-  // const [messages, setMessages] = useState<IMessage[]>([]);
   const socket = initSocket();
 
-  const updateOrAddUser = useCallback(
-    (userId: number, status: string, user?: IListUser) => {
-      const userExist = usersList.find((user) => user.id === userId);
-      if (userExist) {
-        setUsersList(
-          //Update existing user status
-          usersList.map((oldUser) => {
-            if (oldUser.id === userExist.id) {
-              console.log("user exists", userExist, status);
-              return { ...oldUser, status: status };
-            } else return oldUser;
-          })
-        );
-      } else if (user) {
-        setUsersList([...usersList, user]);
-      }
-    },
-    [setUsersList, usersList]
-  );
-
-  useEffect(() => {
-    console.log("Connecting to socket", user);
-    socket.connect();
-    socket.on("connect", () => {
-      console.log("Connected to Socket.IO");
-      socket.emit("subscribe", "chat");
-      console.log("Socket ID", socket.id);
-    });
-
-    socket.on("connect_error", async (err) => {
-      console.log("Socket Connection Error");
-      if (err.message === "invalid token") {
-        const response = await getNewTokenByRefreshToken();
-        // console.log("RefreshTokens", response);
-        if (response.status === 201) {
-          setTimeout(() => {
-            console.log("RECONNECTING>>>>>>");
-            socket.connect();
-          }, 1000);
-        } //else  if(response.status===401){
-        //   NavigateService.navigate("/")
-        // }
-      }
-    });
-
-    return () => {
-      socket.disconnect();
-      socket.off("connect_error");
-    };
-  }, [socket, user]);
-
-  useEffect(() => {
-    socket.on("users", (users: IListUser[]) => {
-      console.log("online Users");
-      setUsersList(users);
-    });
-
-    return () => {
-      socket.off("users");
-    };
-  }, [socket, setUsersList]);
-
-  const setChatMessages = useCallback(
-    async (message: IMessage) => {
-      if (!message.recipientId || message.recipientId === 0) {
-        updateMainChatMessages(message);
-      } else if (message.recipientId && message.recipientId > 0) {
-        const msgConcernedUserId =
-          user?.id === message.senderId
-            ? message.recipientId
-            : message.senderId;
-        const msgUser = await usersList.find(
-          (usr) => usr.id === msgConcernedUserId
-        );
-        if (msgUser) updateUserMessages(msgUser.id, message);
-      }
-      updateActiveChatMessages(message);
-    },
-    [
-      updateActiveChatMessages,
-      usersList,
-      updateMainChatMessages,
-      updateUserMessages,
-      user,
-    ]
-  );
-  useEffect(() => {
-    const setMessageHistory = (messageHistory: IMessage[]) => {
-      console.log("Message History got");
-      if (messageHistory) {
-        setMainChatMessages(messageHistory);
-        setActiveChatMessages(messageHistory);
-      }
-    };
-
-    socket.on("messageHistory", setMessageHistory);
-
-    socket.on("message", setChatMessages);
-
-    return () => {
-      socket.off("message", setChatMessages);
-    };
-  }, [socket, setMainChatMessages, setChatMessages, setActiveChatMessages]);
-
-  useEffect(() => {
-    socket.on("userConnected", (user: IListUser) => {
-      console.log("New User Connected", user);
-      updateOrAddUser(user.id, "online", user);
-    });
-    return () => {
-      socket.off("userConnected");
-    };
-  }, [socket, updateOrAddUser]);
-
-  useEffect(() => {
-    socket.on("userDisconnected", (userId: number) => {
-      console.log("User disconnected", userId);
-      updateOrAddUser(userId, "offline");
-    });
-    return () => {
-      socket.off("userDisconnected");
-    };
-  }, [socket, updateOrAddUser]);
-
-  useEffect(() => {
-    socket.on("messageReceivedByServer", (msgData: any) => {
-      console.log("Message Received by Server",msgData);
-    });
-    return () => {
-      socket.off("messageReceived");
-    };
-  },[socket]);
-
+ 
   //Here index is user id for Private messages and 0 for main chat
   const onChatChange = (index: number) => {
     setActiveIndex(index);
