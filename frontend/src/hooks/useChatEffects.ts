42e55f4cf13411854a7bb9fbc17b505a41f01bc0
Muhammad Ali <muhammadali.hisar@gmail.com>
2024-01-22 22:32:49 +0500

    User private message id update on message delivery

--- frontend/src/hooks/useChatEffects.ts
+++ frontend/src/hooks/useChatEffects.ts
@@ -1,16 +1,10 @@
-import ChatInput from "@/components/chat/ChatInput";
-import ListItem from "@/components/chat/ContactItem";
-import ContactList from "@/components/chat/ContactList";
-import MessageList from "@/components/chat/MessageList";
-import ChatWindowHeader from "@/components/layout/ChatWindowHeader";
 import { useEffect, useState, useCallback } from "react";
 import useUserStore from "@/hooks/useUserStore";
 import useChatStore from "@/hooks/useChatStore";
 import { IListUser } from "@/interfaces/auth.interfaces";
 import { initSocket } from "@/config/socketio";
 import { IMessage } from "@/interfaces/message.interface";
-import MainChatItem from "@/components/chat/MainChatItem";
-import NavigateService from "@/services/navigate";
+
 import { getNewTokenByRefreshToken } from "@/config/axios";
 
 export const useChatEffects = () => {
@@ -19,19 +13,18 @@ export const useChatEffects = () => {
   const usersList = useUserStore((state) => state.usersList);
   const setUsersList = useUserStore((state) => state.setUsersList);
   const updateUserMessages = useUserStore((state) => state.updateUserMessages);
+  const updateMessage = useUserStore((state) => state.updateMessage);
   const {
-    mainChatMessages,
-    activeChatMessages,
     setActiveChatMessages,
     setMainChatMessages,
     updateActiveChatMessages,
     updateMainChatMessages,
     updateMainMessage,
-    updatePrivateMessage,
+    mainChatMessages,
+    activeChatIndex,
+    setActiveChatIndex,
   } = useChatStore();
   const user = useUserStore((state) => state.user);
-  const [activeIndex, setActiveIndex] = useState(0);
-
   const updateOrAddUser = useCallback(
     (userId: number, status: string, user?: IListUser) => {
       const userExist = usersList.find((user) => user.id === userId);
@@ -40,7 +33,6 @@ export const useChatEffects = () => {
           //Update existing user status
           usersList.map((oldUser) => {
             if (oldUser.id === userExist.id) {
-              console.log("user exists", userExist, status);
               return { ...oldUser, status: status };
             } else return oldUser;
           })
@@ -53,22 +45,16 @@ export const useChatEffects = () => {
   );
 
   useEffect(() => {
-    console.log("Connecting to socket", user);
     socket.connect();
     socket.on("connect", () => {
-      console.log("Connected to Socket.IO");
       socket.emit("subscribe", "chat");
-      console.log("Socket ID", socket.id);
     });
 
     socket.on("connect_error", async (err) => {
-      console.log("Socket Connection Error");
       if (err.message === "invalid token") {
         const response = await getNewTokenByRefreshToken();
-        // console.log("RefreshTokens", response);
         if (response.status === 201) {
           setTimeout(() => {
-            console.log("RECONNECTING>>>>>>");
             socket.connect();
           }, 1000);
         } //else  if(response.status===401){
@@ -85,7 +71,6 @@ export const useChatEffects = () => {
 
   useEffect(() => {
     socket.on("users", (users: IListUser[]) => {
-      console.log("online Users");
       setUsersList(users);
     });
 
@@ -96,7 +81,6 @@ export const useChatEffects = () => {
 
   const setChatMessages = useCallback(
     async (message: IMessage) => {
-        console.log("Message received",message)
       if (!message.recipientId || message.recipientId === 0) {
         updateMainChatMessages(message);
       } else if (message.recipientId && message.recipientId > 0) {
@@ -121,7 +105,6 @@ export const useChatEffects = () => {
   );
   useEffect(() => {
     const setMessageHistory = (messageHistory: IMessage[]) => {
-      console.log("Message History got");
       if (messageHistory) {
         setMainChatMessages(messageHistory);
         setActiveChatMessages(messageHistory);
@@ -139,7 +122,6 @@ export const useChatEffects = () => {
 
   useEffect(() => {
     socket.on("userConnected", (user: IListUser) => {
-      console.log("New User Connected", user);
       updateOrAddUser(user.id, "online", user);
     });
     return () => {
@@ -149,7 +131,6 @@ export const useChatEffects = () => {
 
   useEffect(() => {
     socket.on("userDisconnected", (userId: number) => {
-      console.log("User disconnected", userId);
       updateOrAddUser(userId, "offline");
     });
     return () => {
@@ -159,17 +140,32 @@ export const useChatEffects = () => {
 
   useEffect(() => {
     socket.on("pmMessageReceivedByServer", (msgData: any) => {
-      const { messageId, messageClientId, receipientId } = msgData;
-      console.log("Message Received by Server", msgData);
-      updatePrivateMessage(messageId, messageClientId, receipientId);
+      const { messageId, messageClientId, recipientId } = msgData;
+      updateMessage(messageId, messageClientId, recipientId);
+      if (activeChatIndex > 0) {
+        const userMessages = usersList.find((u) => u.id === activeChatIndex);
+        console.log("Active chat user", userMessages)
+        setActiveChatMessages(userMessages?.messages || []);
+      }
     });
-    socket.on("mMessageReceivedByServer", (msgData: any) => {
-      console.log("Message Received by Server", msgData);
-      updateMainMessage(msgData.messageId, msgData.messageClientId);
+    socket.on("mMessageReceivedByServer", async(msgData: any) => {
+      await updateMainMessage(msgData.messageId, msgData.messageClientId);
+      if (activeChatIndex === 0) {
+        console.log("Main Chat message", mainChatMessages)
+        setActiveChatMessages(mainChatMessages);
+      }
     });
     return () => {
       socket.off("pmMessageReceivedByServer");
       socket.off("mMessageReceivedByServer");
     };
-  }, [socket, updateMainMessage, updatePrivateMessage]);
+  }, [
+    socket,
+    updateMainMessage,
+    updateMessage,
+    activeChatIndex,
+    mainChatMessages,
+    setActiveChatMessages,
+    usersList,
+  ]);
 };
--- frontend/src/hooks/useUserStore.ts
+++ frontend/src/hooks/useUserStore.ts
@@ -3,6 +3,8 @@ import { create } from "zustand";
 import { IUser, IListUser } from "@/interfaces/auth.interfaces";
 import { persist } from "zustand/middleware";
 import { IMessage } from "@/interfaces/message.interface";
+import { produce } from "immer";
+
 type UserWithoutPassword = Pick<
   IUser,
   "id" | "username" | "email" | "accessToken" | "refreshToken"
@@ -11,9 +13,13 @@ interface UserStore {
   user: UserWithoutPassword | null;
   usersList: IListUser[];
   setUser: (newUser: UserWithoutPassword) => void;
-  updateUserTokens: (token: string, refToken:string) => void;
   setUsersList: (newList: IListUser[]) => void;
-  updateUserMessages:(userId:number, newMessage:IMessage)=>void
+  updateUserMessages: (userId: number, newMessage: IMessage) => void;
+  updateMessage: (
+    id: number,
+    messageClientId: number,
+    receipientId: number
+  ) => void;
   clearUserStore: () => void;
 }
 
@@ -25,38 +31,49 @@ const useUserStore = create<UserStore>()(
       setUser: (newUser) => {
         set({ user: newUser });
       },
-      updateUserTokens: (accessToken: string, refreshToken:string) => {
-        const oldUser = get().user;
-        if (oldUser?.email && oldUser?.id && oldUser.username) {
-          set({
-            user: {
-              ...oldUser,
-              accessToken: accessToken,
-              refreshToken: refreshToken
-            },
-          });
-        }
-      },
+
       setUsersList: (newList: IListUser[]) => {
         set({
           usersList: newList.map((u) => {
             //TODO: assign latest 10 messages to user in the backend and remove this u.messages
-            u.messages=[] 
+            u.messages = [];
             return u;
           }),
         });
       },
-      updateUserMessages:(userId, newMessage:IMessage)=>{
+
+      //TODO: Convert it to immer middleware, currently getting proxy issue using immer
+      updateMessage: (
+        id: number,
+        messageClientId: number,
+        recipientId: number
+      ): void => {
+        const indexToBeUpdated = get().usersList.findIndex(
+          (u) => u.id === recipientId
+        );
+        if (indexToBeUpdated > -1) {
+          console.log("store recipientId", recipientId);
+          const updatedMessages = get().usersList[
+            indexToBeUpdated
+          ].messages.map((msg) => {
+            if (msg.messageClientId === messageClientId) return { ...msg, id };
+            else return msg;
+          });
+          const newUsersList = [...get().usersList];
+          newUsersList[indexToBeUpdated].messages = updatedMessages;
+          set({ usersList: newUsersList });
+        }
+      },
+      updateUserMessages: (userId, newMessage: IMessage) => {
         set({
-          usersList: get().usersList.map((u)=>{
-            if(u.id===userId) {
-             u.messages=[...u.messages,newMessage];
-             u.isNewMessage= true;
-             return u
-            }
-            else return u;
-          })
-        })
+          usersList: get().usersList.map((u) => {
+            if (u.id === userId) {
+              u.messages = [...u.messages, newMessage];
+              u.isNewMessage = true;
+              return u;
+            } else return u;
+          }),
+        });
       },
       clearUserStore: () => {
         set({ user: null });
