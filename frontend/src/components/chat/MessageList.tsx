ae5e68c94d164d87e6e636f0b501f67220d1a362
Muhammad Ali <muhammadali.hisar@gmail.com>
2024-01-28 18:03:58 +0500

    show message user

--- frontend/src/components/chat/MessageList.tsx
+++ frontend/src/components/chat/MessageList.tsx
@@ -7,23 +7,31 @@ interface IProps {
 const MessageList = ({messages}:IProps) => {
 
   const user= useUserStore(state=>state.user)
+  const isMe=(senderId:number)=>{
+    return senderId===user?.id
+  }
  console.log("Messages",messages)
   return (
     <div className="flex flex-col h-full p-10  overflow-y-auto">
       {messages.map((message:IMessage) => (
         <div
           key={message.id}
-          className={`flex ${message.senderId === user?.id ? 'justify-end' : 'justify-start'} mb-2`}
+          className={`flex ${isMe(message.senderId) ? 'justify-end' : 'justify-start'} mb-2`}
         >
+           <div className='flex flex-col'>
+          {!isMe(message.senderId) && <span className='text-green-500 text-sm'>{message.sender?.username}</span>}
           <div
             className={`${
-              message.senderId === user?.id
-                ? 'bg-green-400 text-white rounded-bl-lg rounded-tl-lg rounded-tr-lg'
+              isMe(message.senderId)
+                ? 'bg-green-500 text-white rounded-bl-lg rounded-tl-lg rounded-tr-lg'
                 : 'bg-gray-200 text-gray-800 rounded-br-lg rounded-bl-lg rounded-tr-lg'
             } py-2 px-4 max-w-sm break-words`}
           >
+           
+         
            {message.id} {message.message}
           </div>
+        </div>
         </div>
       ))}
     </div>
--- frontend/src/interfaces/message.interface.ts
+++ frontend/src/interfaces/message.interface.ts
@@ -1,8 +1,11 @@
+import { IListUser } from "./auth.interfaces";
+
 export interface IMessage {
   id?: number;
   messageClientId?: number;
   message: string;
   senderId: number;
+  sender?:IListUser
   recipientId?: number;
   room?: string;
 }
